38fa629f124649651e0f3e5823b55ee14f9dd1d3
deadboydanne <nds_se@hotmail.com>
2013-10-03 19:51:12 +0200

    commit v0.1.3

---
+++ src/CObject/CObject.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Holding a instance of CLydia to enable use of $this in subclasses.
+ *
+ * @package LydiaCore
+ */
+class CObject {
+
+   public $config;
+   public $request;
+   public $data;
+
+   /**
+    * Constructor
+    */
+   protected function __construct() {
+    $ma = CLydia::Instance();
+    $this->config   = &$ma->config;
+    $this->request  = &$ma->request;
+    $this->data     = &$ma->data;
+  }
+
+}
