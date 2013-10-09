71f1d96d91af3ecca9acc35194347d0e64b0c9f2
deadboydanne <nds_se@hotmail.com>
2013-10-09 23:26:09 +0200

    commit v0.1.9

--- src/CCGuestbook/CCGuestbook.php
+++ src/CCGuestbook/CCGuestbook.php
@@ -4,10 +4,11 @@
  * 
  * @package LydiaCore
  */
-class CCGuestbook extends CObject implements IController, IHasSQL {
+class CCGuestbook extends CObject implements IController {
 
 
-  private $pageTitle = 'Malin Guestbook Example';  
+  private $guestbookModel;
+  
 
 
   /**
@@ -15,34 +16,20 @@ class CCGuestbook extends CObject implements IController, IHasSQL {
    */
   public function __construct() {
     parent::__construct();
+    $this->guestbookModel = new CMGuestbook();
   }
-  
 
-	/**
-	 * Implementing interface IHasSQL. Encapsulate all SQL used by this class.
-	 *
-	 * @param string $key the string that is the key of the wanted SQL-entry in the array.
-	 */
-  public static function SQL($key=null) {
-  	$queries = array(
-  		'create table guestbook'  => "CREATE TABLE IF NOT EXISTS Guestbook (id INTEGER PRIMARY KEY, entry TEXT, created DATETIME default (datetime('now')));",
-  		'insert into guestbook'   => 'INSERT INTO Guestbook (entry) VALUES (?);',
-  		'select * from guestbook' => 'SELECT * FROM Guestbook ORDER BY id DESC;',
-  		'delete from guestbook'   => 'DELETE FROM Guestbook;',
-  	);
-  	if(!isset($queries[$key])) {
-  		throw new Exception("No such SQL query, key '$key' was not found.");
-		}
-		return $queries[$key];
-	}
+
+
 
   /**
    * Implementing interface IController. All controllers must have an index action.
+   * Show a standard frontpage for the guestbook.
    */
   public function Index() {
-    $this->views->SetTitle($this->pageTitle);
+    $this->views->SetTitle('Lydia Guestbook Example');
     $this->views->AddInclude(__DIR__ . '/index.tpl.php', array(
-      'entries'=>$this->ReadAllFromDatabase(), 
+      'entries'=>$this->guestbookModel->ReadAll(), 
       'formAction'=>$this->request->CreateUrl('guestbook/handler')
     ));
   }
@@ -54,64 +41,17 @@ class CCGuestbook extends CObject implements IController, IHasSQL {
    */
   public function Handler() {
     if(isset($_POST['doAdd'])) {
-      $this->SaveNewToDatabase(strip_tags($_POST['newEntry']));
+      $this->guestbookModel->Add(strip_tags($_POST['newEntry']));
     }
     elseif(isset($_POST['doClear'])) {
-      $this->DeleteAllFromDatabase();
-    }            
+      $this->guestbookModel->DeleteAll();
+    }
     elseif(isset($_POST['doCreate'])) {
-      $this->CreateTableInDatabase();
+      $this->guestbookModel->Init();
     }            
     $this->RedirectTo($this->request->CreateUrl($this->request->controller));
   }
   
-  /**
-   * Save a new entry to database.
-   */
-  private function CreateTableInDatabase() {
-    try {
-      $this->db->ExecuteQuery(self::SQL('create table guestbook'));
-      $this->session->AddMessage('success', 'Table created in database.');
-    } catch(Exception$e) {
-      die("$e<br/>Failed to open database: " . $this->config['database'][0]['dsn']);
-    }
-  }
-  
 
-
-  /**
-   * Save a new entry to database.
-   */
-  private function SaveNewToDatabase($entry) {
-    $this->db->ExecuteQuery(self::SQL('insert into guestbook'), array($entry));
-    $this->session->AddMessage('success', 'inserted post into guestbook table.');
-    if($this->db->rowCount() != 1) {
-      $this->session->AddMessage('error', 'Failed to insert new guestbook item into database.');
-	}
-  }
-  
-
-
-  /**
-   * Delete all entries from the database.
-   */
-  private function DeleteAllFromDatabase() {
-    $this->db->ExecuteQuery(self::SQL('delete from guestbook'));
-    $this->session->AddMessage('info', 'Removed all messages from the database table.');
-  }
-  
-  
-  /**
-   * Read all entries from the database.
-   */
-  private function ReadAllFromDatabase() {
-    try {
-      $this->db->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-      return $this->db->ExecuteSelectQueryAndFetchAll(self::SQL('select * from guestbook'));
-    } catch(Exception $e) {
-      return array();    
-    }
-  }
 
 } 
-
