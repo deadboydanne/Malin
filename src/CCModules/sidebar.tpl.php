43dff1d0a5f6262c076ee1ab9fb0c5463e28b29c
deadboydanne <nds_se@hotmail.com>
2013-11-28 20:38:29 +0100

    commit v0.2.24

--- src/CCModules/sidebar.tpl.php
+++ src/CCModules/sidebar.tpl.php
@@ -3,7 +3,7 @@
 <p>All Malin modules.</p>
 <ul>
 <?php foreach($modules as $module): ?>
-  <li><?=$module['name']?></li>
+  <li><a href='<?=create_url("module/view/{$module['name']}")?>'><?=$module['name']?></a></li>
 <?php endforeach; ?>
 </ul>
 </div>
@@ -15,7 +15,7 @@
 <ul>
 <?php foreach($modules as $module): ?>
   <?php if($module['isMalinCore']): ?>
-  <li><?=$module['name']?></li>
+  <li><a href='<?=create_url("module/view/{$module['name']}")?>'><?=$module['name']?></a></li>
   <?php endif; ?>
 <?php endforeach; ?>
 </ul>
@@ -28,7 +28,7 @@
 <ul>
 <?php foreach($modules as $module): ?>
   <?php if($module['isMalinCMF']): ?>
-  <li><?=$module['name']?></li>
+  <li><a href='<?=create_url("module/view/{$module['name']}")?>'><?=$module['name']?></a></li>
   <?php endif; ?>
 <?php endforeach; ?>
 </ul>
@@ -41,7 +41,7 @@
 <ul>
 <?php foreach($modules as $module): ?>
   <?php if($module['isModel']): ?>
-  <li><?=$module['name']?></li>
+  <li><a href='<?=create_url("module/view/{$module['name']}")?>'><?=$module['name']?></a></li>
   <?php endif; ?>
 <?php endforeach; ?>
 </ul>
@@ -54,7 +54,20 @@
 <ul>
 <?php foreach($modules as $module): ?>
   <?php if($module['isController']): ?>
-  <li><?=$module['name']?></li>
+  <li><a href='<?=create_url("module/view/{$module['name']}")?>'><?=$module['name']?></a></li>
+  <?php endif; ?>
+<?php endforeach; ?>
+</ul>
+</div>
+
+
+<div class='box'>
+<h4>Manageable module</h4>
+<p>Implements interface <code>IModule</code>.</p>
+<ul>
+<?php foreach($modules as $module): ?>
+  <?php if($module['isManageable']): ?>
+  <li><a href='<?=create_url("module/view/{$module['name']}")?>'><?=$module['name']?></a></li>
   <?php endif; ?>
 <?php endforeach; ?>
 </ul>
@@ -67,7 +80,7 @@
 <ul>
 <?php foreach($modules as $module): ?>
   <?php if($module['hasSQL']): ?>
-  <li><?=$module['name']?></li>
+  <li><a href='<?=create_url("module/view/{$module['name']}")?>'><?=$module['name']?></a></li>
   <?php endif; ?>
 <?php endforeach; ?>
 </ul>
@@ -80,7 +93,7 @@
 <ul>
 <?php foreach($modules as $module): ?>
   <?php if(!($module['isController'] || $module['isMalinCore'] || $module['isMalinCMF'])): ?>
-  <li><?=$module['name']?></li>
+  <li><a href='<?=create_url("module/view/{$module['name']}")?>'><?=$module['name']?></a></li>
   <?php endif; ?>
 <?php endforeach; ?>
 </ul>
